c255c1f19ddc5087fb4697b89759c4332b5f26ba
Aitor Bernal <alu0100830064@ull.edu.es>
2016-11-15 16:02:21 +0000

    /tmp con cifrado de pasword listo

---
+++ tmp/hash.js
@@ -0,0 +1,12 @@
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 'hola';
+const someOtherPlaintextPassword = 'not_bacon';
+
+var salt = bcrypt.genSaltSync(saltRounds);
+var hash = bcrypt.hashSync(myPlaintextPassword, salt);
+
+var a = bcrypt.compareSync('hola', hash); // true 
+
+
+console.log(a);
