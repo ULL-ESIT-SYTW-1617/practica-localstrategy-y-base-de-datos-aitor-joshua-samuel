778f8437d27140729c8bc183d416e22705324276
Samuel Ramos <alu0100815146@ull.edu.es>
2016-11-24 20:56:06 +0000

    Añadiendo MySql

--- tmp/prueba.js
+++ tmp/prueba.js
@@ -1,48 +1,17 @@
-var json = require('json-update');
-var bcrypt = require('bcrypt');
-var fs = require('fs-extended');
-var readjson = require('readjson');
-var Dropbox = require('dropbox');
-var Fs = require('fs');
-var path = require('path');
-var Sequelize=require('sequelize')
+var mysql = require('mysql');
+var dataBase = require('./dataBase.json');
 
-var sequelize = new Sequelize('SYTW', 'root', 'aitorjoshuasamuel', {
-  host: 'localhost',
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
+var connection = mysql.createConnection({
+  host     : dataBase.dbHost,
+  user     : dataBase.dbUser,
+  password : dataBase.dbPassword,
+  database : dataBase.dbDatabase
 });
 
-var login = sequelize.define('user', {
-  
-  id: {
-    type: Sequelize.NUMBER,
-    field: 'id'
-  },
-  login: {
-    type: Sequelize.STRING,
-    field: 'login'
-  },
+connection.connect();
 
-  name: {
-    type: Sequelize.STRING,
-    field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
-  }
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
 
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
+  console.log('The solution is: ', rows[0].solution);
 });
-
-User.sync({force: true}).then(function () {
-  // Table created
-  return User.create({
-    name: 'admin',
-    id: 1
-  });
-});
-
